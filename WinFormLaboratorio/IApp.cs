ea07e6d3efe959375f0598263fc62d3a498914ef
Andrey Salazar Guzman <asalazar@grupomas.com>
2021-05-30 22:49:05 -0600

    Add project files.

---
+++ WinFormLaboratorio/IApp.cs
@@ -0,0 +1,18 @@
+﻿using BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormLaboratorio
+{
+   public struct IApp
+    {
+
+        public static IPruebaService PruebaService => new PruebaService();
+
+
+
+    }
+}
