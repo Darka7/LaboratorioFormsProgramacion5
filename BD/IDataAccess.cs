abcf2d2b523b3544bdeb897818720a2d5a2be561
Andrey Salazar Guzman <asalazar@grupomas.com>
2021-06-14 19:12:45 -0600

    mejoras

--- BD/IDataAccess.cs
+++ BD/IDataAccess.cs
@@ -7,12 +7,12 @@ namespace BD
     {
         DBEntity Execute(string sp, object Param = null, int? Timeout = null);
         IEnumerable<dynamic> Query(string sp, object Param = null, int? Timeout = null);
-        IEnumerable<A> Query<A, B, C, D, E, H, I>(string sp, object Param = null, int? Timeout = null);
-        IEnumerable<A> Query<A, B, C, D, E, H>(string sp, object Param = null, int? Timeout = null);
-        IEnumerable<A> Query<A, B, C, D, E>(string sp, object Param = null, int? Timeout = null);
-        IEnumerable<A> Query<A, B, C, D>(string sp, object Param = null, int? Timeout = null);
-        IEnumerable<A> Query<A, B, C>(string sp, object Param = null, int? Timeout = null);
-        IEnumerable<A> Query<A, B>(string sp, object Param = null, int? Timeout = null);
+        IEnumerable<A> Query<A, B, C, D, E, H, I>(string sp, string split, object Param = null, int? Timeout = null);
+        IEnumerable<A> Query<A, B, C, D, E, H>(string sp, string split, object Param = null, int? Timeout = null);
+        IEnumerable<A> Query<A, B, C, D, E>(string sp, string split, object Param = null, int? Timeout = null);
+        IEnumerable<A> Query<A, B, C, D>(string sp, string split, object Param = null, int? Timeout = null);
+        IEnumerable<A> Query<A, B, C>(string sp, string split, object Param = null, int? Timeout = null);
+        IEnumerable<A> Query<A, B>(string sp, string split, object Param = null, int? Timeout = null);
         IEnumerable<T> Query<T>(string sp, object Param = null, int? Timeout = null);
         T QueryFirst<T>(string sp, object Param = null, int? Timeout = null);
     }
